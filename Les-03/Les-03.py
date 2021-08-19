26a8ee3e394aeb30b8fb19a1da69bf6e174f4f9d
red-F0X <stas-panin@mail.ru>
2021-08-19 15:14:36 +0300

    Add сomments for the funcs.

--- Les-03/Les-03.py
+++ Les-03/Les-03.py
@@ -4,6 +4,12 @@
 
 # В диапазоне натуральных чисел от 2 до 99 определить, сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
 def count_multiplicity_nums(start: int, end: int):
+    """
+    Find the multiplicity of numbers in a range.
+    :param start:
+    :param end:
+    :return:
+    """
     result = {}
     for num_multiplicity in range(2, 10):
         result[num_multiplicity] = []
@@ -18,28 +24,53 @@ def count_multiplicity_nums(start: int, end: int):
 # во второй массив надо заполнить значениями 1, 4, 5, 6 (или 0, 3, 4, 5 - если индексация начинается с нуля),
 # т.к. именно в этих позициях первого массива стоят четные числа.
 def even_element_list(ls: list):
+    """
+    Create list of indices of even elements of the origin list.
+    :param ls:
+    :return:
+    """
     return [ls.index(el) for el in ls if el % 2 == 0]
 
 
 # В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 def change_max_min(ls: list):
+    """
+    Change index max and min nums.
+    :param ls:
+    :return:
+    """
     ls[ls.index(max(ls))], ls[ls.index(min(ls))] = ls[ls.index(min(ls))], ls[ls.index(max(ls))]
     return ls
 
 
 # Определить, какое число в массиве встречается чаще всего.
 def count_max_num(ls: list):
+    """
+    Find a max num and count repeat.
+    :param ls:
+    :return:
+    """
     return max(ls), ls.count(max(ls))
 
 
 # В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.
 def count_min_num(ls: list):
+    """
+    Find a min num and index.
+    :param ls:
+    :return:
+    """
     return min(ls), ls.index(min(ls))
 
 
 # В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.
 # Сами минимальный и максимальный элементы в сумму не включать.
 def range_minmax_sum_ls(ls: list):
+    """
+    Find the sum of elements array in a range.
+    :param ls:
+    :return:
+    """
     step = 1
     min_index, max_index = ls.index(min(ls)), ls.index(max(ls))
     if max_index - min_index < 0:
@@ -53,6 +84,11 @@ def range_minmax_sum_ls(ls: list):
 # В одномерном массиве целых чисел определить два наименьших элемента.
 # Они могут быть как равны между собой (оба являться минимальными), так и различаться.
 def two_min_el(ls: list):
+    """
+    Find two min nums.
+    :param ls:
+    :return:
+    """
     min_num1 = min(ls)
     ls.pop(ls.index(min(ls)))
     return min_num1, min(ls)
@@ -62,6 +98,10 @@ def two_min_el(ls: list):
 # Программа должна вычислять сумму введенных элементов каждой строки и записывать ее в последнюю ячейку строки.
 # В конце следует вывести полученную матрицу.
 def matrix_from_keyboard():
+    """
+    Create matrix from keyboard and print him.
+    :return:
+    """
     matrix = []
     print('Matrix 5x4. Please enter numbers, for sum 4 elements in row.')
     for row in range(0, 4):
@@ -81,6 +121,11 @@ def matrix_from_keyboard():
 
 # Найти максимальный элемент среди минимальных элементов столбцов матрицы.
 def max_min_el_matrix(ls: list):
+    """
+    Find the maximum element among the minimum elements.
+    :param ls:
+    :return:
+    """
     import numpy as np
     return max([min(el) for el in np.transpose(ls)])
 
